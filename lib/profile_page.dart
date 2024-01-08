820c50f4fd812e005a18525c446cf3e2122f9a7f
jananiwijedeva <janani.wijayantha3.com>
2024-01-08 14:45:18 +0530

    modified:   lib/profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:g4c/asset_widgets.dart';
 
 class ProfilePage extends StatelessWidget {
+  final double fontsize = 15.0;
+
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Profile Page');
+    return ListView(
+      children: [
+        Column(
+          children: [
+            const TopCard(
+              content: Image(
+                fit: BoxFit.fill,
+                image: AssetImage('asset_lib/images/back.png'),
+              ),
+              color: Color.fromARGB(255, 195, 255, 195),
+            ),
+            CardWidget(
+              content: Container(),
+              title: 'Personality Type',
+              fontsize: fontsize,
+            ),
+            CardWidget(
+              content: Container(),
+              title: 'Traits',
+              fontsize: fontsize,
+            ),
+            CardWidget(
+              content: Container(),
+              title: 'Skill Level',
+              fontsize: fontsize,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
